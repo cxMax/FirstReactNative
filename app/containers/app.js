7c128b5c316230c54b2e7d16a7b4df781558a392
cxMax <caixi169@hotmail.com>
2016-08-09 01:38:02 +0800

    '增加navigator.js'

--- app/containers/app.js
+++ app/containers/app.js
@@ -3,3 +3,74 @@
  */
 import React from 'react-native';
 import LoadingViewAndroid from  '../components/LoadingView';
+import {ToastShort} from '../utils/ToastUtils';
+import {NaviGoBack} from '../utils/CommonUtils';
+
+const {
+    StyleSheet,
+    Navigator,
+    StatusBar,
+    BackAndroid,
+    View
+    } = React;
+var _navigator, isRemoved = false;
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.renderScene = this.renderScene.bind(this);
+    }
+
+    goBack() {
+        return NaviGoBack(_navigator);
+    }
+
+    renderScene(route, navigator) {
+        let Component = route.component;
+        _navigator = navigator;
+        if (route.name === 'WebViewPage') {
+            BackAndroid.removeEventListener('hardwareBackPress', this.goBack);
+            isRemoved = true;
+        } else {
+            if (isRemoved) {
+                BackAndroid.addEventListener('hardwareBackPress', this.goBack);
+            }
+        }
+        return (
+            <Component navigator={navigator} route={route}/>
+        );
+    }
+
+    configureScene(route, routeStack) {
+        return Navigator.SceneConfigs.PushFromRight;
+    }
+
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <StatusBar
+                    backgroundColor="#FF0000"
+                    barStyle="default"
+                    />
+                <Navigator
+                    ref='nabigator'
+                    styles={styles.navigator}
+                    configureScene={this.configureScene}
+                    renderScene={this.renderScene}
+                    initialRoute={{
+                        component: Splash,
+                        name: 'Splash'
+                      }}
+                    />
+            </View>
+        );
+    }
+}
+
+let styles = StyleSheet.create({
+    navigator: {
+        flex: 1
+    }
+});
+
+export default App;
